bace39ad7727ac186d75477a7ff5f7cd253fb4fe
Adrienne Walker <enne@quisquo.us>
2017-09-27 22:19:34 -0700

    Fix tabs and spaces oops

--- raidboss.js
+++ raidboss.js
@@ -33,46 +33,46 @@ Options.Triggers = [
     zoneRegex: /./,
     triggers: [
       {
-	id: 'Provoke',
-	regex: /:(\y{Name}):1D6D:Provoke:/,
-	infoText: function(data, matches) {
-	  return 'Provoke: ' + matches[1];
-	},
+        id: 'Provoke',
+        regex: /:(\y{Name}):1D6D:Provoke:/,
+        infoText: function(data, matches) {
+          return 'Provoke: ' + matches[1];
+        },
       },
       {
-	id: 'Shirk',
-	regex: /:(\y{Name}):1D71:Shirk:/,
-	infoText: function(data, matches) {
-	  return 'Shirk: ' + matches[1];
-	},
+        id: 'Shirk',
+        regex: /:(\y{Name}):1D71:Shirk:/,
+        infoText: function(data, matches) {
+          return 'Shirk: ' + matches[1];
+        },
       },
       {
-	id: 'Holmgang',
-	regex: /:(\y{Name}):2B:Holmgang:/,
-	infoText: function(data, matches) {
-	  return 'Holmgang: ' + matches[1];
-	},
+        id: 'Holmgang',
+        regex: /:(\y{Name}):2B:Holmgang:/,
+        infoText: function(data, matches) {
+          return 'Holmgang: ' + matches[1];
+        },
       },
       {
-	id: 'Hallowed',
-	regex: /:(\y{Name}):1E:Hallowed Ground:/,
-	infoText: function(data, matches) {
-	  return 'Hallowed: ' + matches[1];
-	},
+        id: 'Hallowed',
+        regex: /:(\y{Name}):1E:Hallowed Ground:/,
+        infoText: function(data, matches) {
+          return 'Hallowed: ' + matches[1];
+        },
       },
       {
-	id: 'Living',
-	regex: /:(\y{Name}):E36:Living Dead:/,
-	infoText: function(data, matches) {
-	  return 'Living: ' + matches[1];
-	},
+        id: 'Living',
+        regex: /:(\y{Name}):E36:Living Dead:/,
+        infoText: function(data, matches) {
+          return 'Living: ' + matches[1];
+        },
       },
       {
-	id: 'Walking',
-	regex: /:(\y{Name}) gains the effect of Walking Dead/,
-	infoText: function(data, matches) {
-	  return 'Walking: ' + matches[1];
-	},
+        id: 'Walking',
+        regex: /:(\y{Name}) gains the effect of Walking Dead/,
+        infoText: function(data, matches) {
+          return 'Walking: ' + matches[1];
+        },
       },
     ]
   },
