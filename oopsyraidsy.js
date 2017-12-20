a1945ce8f744ec7ceb08e4944640b8e67df86f47
Adrienne Walker <enne@quisquo.us>
2017-12-20 14:13:58 -0800

    Add nicks for oopsy raidsy

---
+++ oopsyraidsy.js
@@ -0,0 +1,9 @@
+Options.PlayerNicks = {
+  'Paprika Rika': 'Pap',
+  'Elmindreda Farshaw': 'Min',
+  'Mikoto Misaka': 'Miko',
+  'Lohpopo Lopopo': 'Loh',
+  'Ninene Nine': 'Nine',
+  'Wikiki Wiki': 'Wiki',
+  'Kinono Kino': 'Kino',
+};
