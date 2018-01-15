08ca50a4f3fb7ac47ccce41f05f2fe7ab97615ae
Adrienne Walker <enne@quisquo.us>
2018-01-15 14:11:14 -0800

    raidboss: make tank callouts only for non-tanks

--- raidboss.js
+++ raidboss.js
@@ -6,50 +6,50 @@ Options.Triggers = [
     zoneRegex: /.*/,
     triggers: [
       {
-        id: 'Provoke',
         regex: /:(\y{Name}):1D6D:Provoke:/,
+        condition: function(data) { return data.role != 'tank' },
         infoText: function(data, matches) {
           return 'Provoke: ' + matches[1];
         },
       },
       {
-        id: 'Ultimatum',
         regex: /:(\y{Name}):1D73:Ultimatum:/,
+        condition: function(data) { return data.role != 'tank' },
         infoText: function(data, matches) {
           return 'Ultimatum: ' + matches[1];
         },
       },
       {
-        id: 'Shirk',
         regex: /:(\y{Name}):1D71:Shirk:/,
+        condition: function(data) { return data.role != 'tank' },
         infoText: function(data, matches) {
           return 'Shirk: ' + matches[1];
         },
       },
       {
-        id: 'Holmgang',
         regex: /:(\y{Name}):2B:Holmgang:/,
+        condition: function(data) { return data.role != 'tank' },
         infoText: function(data, matches) {
           return 'Holmgang: ' + matches[1];
         },
       },
       {
-        id: 'Hallowed',
         regex: /:(\y{Name}):1E:Hallowed Ground:/,
+        condition: function(data) { return data.role != 'tank' },
         infoText: function(data, matches) {
           return 'Hallowed: ' + matches[1];
         },
       },
       {
-        id: 'Living',
         regex: /:(\y{Name}):E36:Living Dead:/,
+        condition: function(data) { return data.role != 'tank' },
         infoText: function(data, matches) {
           return 'Living: ' + matches[1];
         },
       },
       {
-        id: 'Walking',
         regex: /:(\y{Name}) gains the effect of Walking Dead/,
+        condition: function(data) { return data.role != 'tank' },
         infoText: function(data, matches) {
           return 'Walking: ' + matches[1];
         },
